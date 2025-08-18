5aa0abab21f786e3f29da114b2e47de897267d3a
unknown <manishmania20@gmail.com>
2025-08-18 19:50:48 +0530

    added module exports

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-// const isProd = process.env.NODE_ENV === 'production';
-
-// const nextConfig = {
-//   output: 'export',
-//   basePath: isProd ? '/portfolio_react' : '',
-//   assetPrefix: isProd ? '/portfolio_react/' : '',
-//   images: { unoptimized: true },
-// };
-
-// module.exports = nextConfig;
+module.exports = {
+  reactStrictMode: true,
+}
