caf1592bb8764fe56e83b1c5f9678a6cda265a92
unknown <manishmania20@gmail.com>
2025-08-18 19:13:50 +0530

    added ci/cd pipeline with github actions with changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// const isProd = process.env.NODE_ENV === 'production';
+
+// const nextConfig = {
+//   output: 'export',
+//   basePath: isProd ? '/portfolio_react' : '',
+//   assetPrefix: isProd ? '/portfolio_react/' : '',
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
